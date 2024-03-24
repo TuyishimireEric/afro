4ca2fedd3be38645f864a7a2b608354a31417380
Mukunzijames <mukunzindahiro@gmail.com>
2024-03-24 18:20:26 +0200

    changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
     <div className="navbar-section">
       <h1 className="navbar-title">
         <Link to="/">
-          Afro-Synegy <span className="navbar-sign"></span>
+          Afro-Synergy <span className="navbar-sign"></span>
         </Link>
       </h1>
 
